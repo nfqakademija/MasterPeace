9c0203e5b4c19cea645a90edb0e87d46358626b4
Karolis <karolismatjosaitis@gmail.com>
2016-10-17 18:11:20 +0300

    delete<netbeans, controler>removed neatbeans project directory, dev template controler and twig

--- src/AppBundle/Controller/templateController.php
+++ src/AppBundle/Controller/templateController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: indel
+ * User: Karolis Matjosaitis
  * Date: 10/15/16
  * Time: 10:25 PM
  */
@@ -10,15 +10,25 @@ namespace AppBundle\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class templateController
+# Templating service, renders templates.
+# base Controller gets access to service container.
+
+class templateController extends Controller
 {
     /**
      * @Route("/template/{templateName}")
      */
     public function showAction($templateName)
     {
-        return new Response("Template for pages: ".$templateName);
+        # gets templating service. Container has only one method: get(). Give nickname to service and it
+        # it will return that object.
+        #render template:
+        return $this->render('template/show.html.twig', [
+            # passing variables
+            'name' => $templateName
+        ]);
     }
 }
