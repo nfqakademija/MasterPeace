a3b01c3f5aaa20696d9d410f2520d07daaf2ea8a
karolis <karolis@thermo>
2016-10-18 16:00:44 +0300

    develop<CRUDE>add user bundle, css and js need's to be fixed.

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('password')->add('firstName')->add('lastName')->add('email')->add('lastLogin')->add('note')->add('created')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_user';
+    }
+
+
+}
