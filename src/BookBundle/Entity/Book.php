b975ee873ae44f7fa30a3c994a70f837ea4d8138
Sergej Voronov <sergej@voronov.lt>
2016-10-22 03:49:03 +0300

    develop<BookBundle>:create BookBundle Entity

---
+++ src/BookBundle/Entity/Book.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace BookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity
+ */
+
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $publisher;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cover;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $isbn_code;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set year
+     *
+     * @param integer $year
+     *
+     * @return Book
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return integer
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Set publisher
+     *
+     * @param string $publisher
+     *
+     * @return Book
+     */
+    public function setPublisher($publisher)
+    {
+        $this->publisher = $publisher;
+
+        return $this;
+    }
+
+    /**
+     * Get publisher
+     *
+     * @return string
+     */
+    public function getPublisher()
+    {
+        return $this->publisher;
+    }
+
+    /**
+     * Set cover
+     *
+     * @param string $cover
+     *
+     * @return Book
+     */
+    public function setCover($cover)
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    /**
+     * Get cover
+     *
+     * @return string
+     */
+    public function getCover()
+    {
+        return $this->cover;
+    }
+
+    /**
+     * Set isbnCode
+     *
+     * @param integer $isbnCode
+     *
+     * @return Book
+     */
+    public function setIsbnCode($isbnCode)
+    {
+        $this->isbn_code = $isbnCode;
+
+        return $this;
+    }
+
+    /**
+     * Get isbnCode
+     *
+     * @return integer
+     */
+    public function getIsbnCode()
+    {
+        return $this->isbn_code;
+    }
+}
