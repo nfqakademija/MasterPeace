162fc7a7615fc32e548606b788e80f033c70ca7e
karolis <karolis@thermo>
2016-10-25 11:16:23 +0300

    fix<UserBundle> restored, added server configuratio for dbal

---
+++ src/MasterPeace/Bundle/UserBundle/Controller/AllFosUsers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MasterPeace\Bundle\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AllFosUsers extends Controller
+{
+    /**
+     * @Route("/allusers")
+     */
+    public function indexAction()
+    {
+        $repository = $this->getDoctrine()
+            ->getRepository('MasterPeaceUserBundle:User');
+
+        $users = $repository->findAll();
+        return $this->render('', array('users' => $users));
+    }
+
+}
