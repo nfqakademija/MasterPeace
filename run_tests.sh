dcc83d76c399958d5005360b5b422bd9f38990c7
Karolis <karolismatjosaitis@gmail.com>
2016-11-03 23:08:19 +0200

    extention<run-local-test>: bash script for local testing with coverall

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-php bin/console cache:clear &&
 composer install &&
+php bin/console cache:clear &&
 php bin/console doctrine:schema:drop --force &&
 php bin/console doctrine:schema:create &&
 php bin/console doctrine:fixtures:load --no-interaction &&
 
-vendor/bin/phpcs --standard=PSR2 src
-vendor/bin/phpunit
+vendor/bin/phpcs --standard=PSR2 src &&
+vendor/bin/phpunit --coverage-text --coverage-clover build/logs/clover.xml
