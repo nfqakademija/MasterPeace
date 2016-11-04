4466af59496d3df983e92c27b136be8f139bf83b
Karolis <karolismatjosaitis@gmail.com>
2016-11-04 19:51:39 +0200

    fix<routing> deleted route for non egzisting controller

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-php bin/console cache:clear &&
 composer install &&
+php bin/console cache:clear &&
+
 php bin/console doctrine:schema:drop --force &&
 php bin/console doctrine:schema:create &&
 php bin/console doctrine:fixtures:load --no-interaction &&
