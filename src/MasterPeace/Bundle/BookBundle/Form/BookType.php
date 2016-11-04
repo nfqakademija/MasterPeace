ad38297e282f25cdf9e5e4358ece197f58dc5f81
Sergej Voronov <sergej@voronov.lt>
2016-11-04 02:00:56 +0200

    develop<BookType>: add various field types, add data_class

--- src/MasterPeace/Bundle/BookBundle/Form/BookType.php
+++ src/MasterPeace/Bundle/BookBundle/Form/BookType.php
@@ -3,20 +3,33 @@
 namespace MasterPeace\Bundle\BookBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('author')
-            ->add('year')
-            ->add('publisher')
-            ->add('cover')
-            ->add('isbnCode')
-            ->add('save', SubmitType::class);
+            ->add('title', TextType::class)
+            ->add('author', TextType::class)
+            ->add('year', IntegerType::class)
+            ->add('publisher', TextType::class)
+            ->add('cover', FileType::class, [
+                'required' => false
+            ])
+            ->add('isbnCode', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Add New Book'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MasterPeace\Bundle\BookBundle\Entity\Book',
+        ));
     }
 }
