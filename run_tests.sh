8a5b1bcce8c0b183207bfda09ad4294503389f46
Sergej Voronov <sergej@voronov.lt>
2016-11-05 17:54:06 +0200

    fix<QuizEntity>: attribute name (question) to plural, other minor fixes

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,9 @@
 
 composer install &&
 php bin/console cache:clear &&
-
 php bin/console doctrine:schema:drop --force &&
 php bin/console doctrine:schema:create &&
 php bin/console doctrine:fixtures:load --no-interaction &&
 
-vendor/bin/phpcs --standard=PSR2 src
-vendor/bin/phpunit
+vendor/bin/phpcs --standard=PSR2 src &&
+vendor/bin/phpunit --coverage-text
