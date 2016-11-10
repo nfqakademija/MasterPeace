c125fae7cc341567b330fdf93b7eae6092d76d7c
Karolis <karolismatjosaitis@gmail.com>
2016-11-10 22:57:19 +0200

    develop<db>: Classroom entity

---
+++ src/MasterPeace/Bundle/UserBundle/Entity/Classroom.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MasterPeace\Bundle\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Classroom
+ *
+ * @ORM\Table(name="classroom")
+ * @ORM\Entity(repositoryClass="MasterPeace\Bundle\UserBundle\Repository\ClassroomRepository")
+ */
+class Classroom
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string")
+     */
+    private $title;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="MasterPeace\Bundle\UserBundle\Entity\User")
+     */
+    private $user;
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return Classroom
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     * @return $this
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
