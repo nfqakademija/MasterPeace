de1da544b62ddf31c2a324d404a96ede68e1f67b
Karolis <karolismatjosaitis@gmail.com>
2016-11-13 03:19:39 +0200

    develop<Conroller>: homepageController and routing with redirections for admin page

--- src/MasterPeace/Bundle/UpReadBundle/Controller/HomePageController.php
+++ src/MasterPeace/Bundle/UpReadBundle/Controller/HomePageController.php
@@ -9,19 +9,43 @@ class HomePageController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function redirectAction()
+    public function redirectUserAction()
     {
-        if ($this->getUser()->isAdmin()) {
-            return $this->render('MasterPeaceUpReadBundle::admin.html.twig');
-        }
+        $response = null;
 
         if ($this->getUser()->isTeacher()) {
-            return $this->render('MasterPeaceUpReadBundle::teacher.html.twig');
+            $response = $this->render('MasterPeaceUpReadBundle::teacher.html.twig');
         }
 
         if ($this->getUser()->isStudent()) {
-            return $this->render('MasterPeaceUpReadBundle::student.html.twig');
+            $response = $this->render('MasterPeaceUpReadBundle::student.html.twig');
         }
+
+        if ($this->getUser()->isAdmin()) {
+            $response = $this->render('MasterPeaceUpReadBundle::admin.html.twig');
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function redirectAdminAction()
+    {
+        $response = null;
+
+        if ($this->getUser()->isAdmin()) {
+            $response = $this->render('MasterPeaceUpReadBundle::admin.html.twig');
+        } else {
+            $response = $this->render('MasterPeaceUserBundle::layout.html.twig');
+        }
+
+        return $response;
     }
 }
