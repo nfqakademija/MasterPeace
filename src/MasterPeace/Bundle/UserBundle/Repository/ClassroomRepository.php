ea37757fe3ee5019f6c45c8eca8d551327357960
Sergej Voronov <sergej@voronov.lt>
2016-11-21 20:11:20 +0200

    fix<Classroom> remove uneeded code

--- src/MasterPeace/Bundle/UserBundle/Repository/ClassroomRepository.php
+++ src/MasterPeace/Bundle/UserBundle/Repository/ClassroomRepository.php
@@ -2,18 +2,8 @@
 
 namespace MasterPeace\Bundle\UserBundle\Repository;
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 
 class ClassroomRepository extends EntityRepository
 {
-    /**
-     * @var EntityManager
-     */
-    private $entityManager;
-
-    public function __constructor(EntityManager $em)
-    {
-        $this->entityManager = $em;
-    }
 }
